b61565e67ff1ad0ca5e081c7871c8e9bc508b6ed
Abdessamed Mansouri <Abdessamed.mansouri@lacombedulionvert.fr>
2021-01-04 15:15:11 +0100

    Implementation de History and Transactions

--- src/main/java/fr/lcdlv/kata/bank/DepositTransaction.java
+++ src/main/java/fr/lcdlv/kata/bank/DepositTransaction.java
@@ -1,6 +1,25 @@
 package fr.lcdlv.kata.bank;
 
+import java.util.Objects;
+
 public class DepositTransaction implements Transaction {
+
+    private Money amount;
+
     public DepositTransaction(Money amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DepositTransaction that = (DepositTransaction) o;
+        return amount.equals(that.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
     }
 }
