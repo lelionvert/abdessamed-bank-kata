df9747a12830b48df462ec4a37ea008e44928dcd
Abdessamed Mansouri <Abdessamed.mansouri@lacombedulionvert.fr>
2021-01-04 11:59:37 +0100

    Refactoring

--- src/main/java/fr/lcdlv/kata/bank/MinimumMoneyAllowedException.java
+++ src/main/java/fr/lcdlv/kata/bank/MinimumMoneyAllowedException.java
@@ -1,4 +1,18 @@
 package fr.lcdlv.kata.bank;
 
 public class MinimumMoneyAllowedException extends Exception {
+    public MinimumMoneyAllowedException() {
+    }
+
+    public MinimumMoneyAllowedException(String message) {
+        super(message);
+    }
+
+    public MinimumMoneyAllowedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MinimumMoneyAllowedException(Throwable cause) {
+        super(cause);
+    }
 }
