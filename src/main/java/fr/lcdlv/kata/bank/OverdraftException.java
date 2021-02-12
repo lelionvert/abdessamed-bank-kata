70dbe0ce0fb4c373290078182bfc5638fa9fc9ad
Abdessamed Mansouri <Abdessamed.mansouri@lacombedulionvert.fr>
2021-02-12 01:43:27 +0100

    Refacto

--- src/main/java/fr/lcdlv/kata/bank/OverdraftException.java
+++ src/main/java/fr/lcdlv/kata/bank/OverdraftException.java
@@ -3,16 +3,4 @@ package fr.lcdlv.kata.bank;
 public class OverdraftException extends Exception {
     public OverdraftException() {
     }
-
-    public OverdraftException(String message) {
-        super(message);
-    }
-
-    public OverdraftException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public OverdraftException(Throwable cause) {
-        super(cause);
-    }
 }
