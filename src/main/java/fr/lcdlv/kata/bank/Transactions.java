57d55bfabddac516697d14ed8beb60cb7535728c
Abdessamed Mansouri <Abdessamed.mansouri@lacombedulionvert.fr>
2021-02-20 05:59:23 +0100

    Réduction de la visibilité de Transactions.transactions

--- src/main/java/fr/lcdlv/kata/bank/Transactions.java
+++ src/main/java/fr/lcdlv/kata/bank/Transactions.java
@@ -8,7 +8,7 @@ import static java.util.stream.Collectors.joining;
 
 public class Transactions {
 
-    protected final List<Transaction> transactions = new ArrayList<>();
+    private final List<Transaction> transactions = new ArrayList<>();
 
     public int size() {
         return transactions.size();
