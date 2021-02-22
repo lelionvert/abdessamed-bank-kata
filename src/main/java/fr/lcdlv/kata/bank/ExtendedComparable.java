d1001d8562e8b227948f6b0fa90c398f896c148d
Abdessamed Mansouri <Abdessamed.mansouri@lacombedulionvert.fr>
2021-02-22 01:51:02 +0100

    Refacto: Utilisation des expressions lambdas

--- src/main/java/fr/lcdlv/kata/bank/ExtendedComparable.java
+++ src/main/java/fr/lcdlv/kata/bank/ExtendedComparable.java
@@ -1,5 +1,7 @@
 package fr.lcdlv.kata.bank;
 
+import java.util.function.Predicate;
+
 public interface ExtendedComparable<T> extends Comparable<T> {
 
     default boolean compareTo(T o, LogicOperation operation) {
@@ -8,43 +10,21 @@ public interface ExtendedComparable<T> extends Comparable<T> {
     }
 
     enum LogicOperation {
-        EQUAL_TO {
-            @Override
-            public boolean test(int result) {
-                return result == 0;
-            }
-        },
-        NOT_EQUAL_TO {
-            @Override
-            public boolean test(int result) {
-                return result != 0;
-            }
-        },
-        BIGGER_THAN {
-            @Override
-            public boolean test(int result) {
-                return result > 0;
-            }
-        },
-        BIGGER_THAN_OR_EQUAL_TO {
-            @Override
-            public boolean test(int result) {
-                return BIGGER_THAN.test(result) || EQUAL_TO.test(result);
-            }
-        },
-        LESS_THAN {
-            @Override
-            public boolean test(int result) {
-                return result < 0;
-            }
-        },
-        LESS_THAN_OR_EQUAL_TO {
-            @Override
-            public boolean test(int result) {
-                return LESS_THAN.test(result) || LESS_THAN.test(result);
-            }
-        };
+        EQUAL_TO(result -> result == 0),
+        NOT_EQUAL_TO(result -> result != 0),
+        BIGGER_THAN(result -> result > 0),
+        BIGGER_THAN_OR_EQUAL_TO(result -> BIGGER_THAN.test(result) || EQUAL_TO.test(result)),
+        LESS_THAN(result -> result < 0),
+        LESS_THAN_OR_EQUAL_TO(result -> LESS_THAN.test(result) || LESS_THAN.test(result));
 
-        public abstract boolean test(int result);
+        private final Predicate<Integer> predicate;
+
+        LogicOperation(final Predicate<Integer> predicate) {
+            this.predicate = predicate;
+        }
+
+        public boolean test(int result) {
+            return predicate.test(result);
+        }
     }
 }
